6271c74176406cbf677536dee100b14f830de2ab
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2021-05-28 10:36:49 +0200

    First commit

---
+++ Create/run.ps1
@@ -0,0 +1,94 @@
+using namespace System.Net
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+.\PSfunctions.ps1
+if (-not [string]::IsNullOrEmpty($ENV:LogoImage)) {
+  $Logo = "<img src=`"$($ENV:LogoImage)`" alt=`"Logo`">"
+}
+else {
+  $Logo = ""
+}
+
+$Username = ([System.Web.HttpUtility]::ParseQueryString($Request.Body)).username
+
+if ($Username) {
+  $MFARequest = New-MFARequest -EmailToPush $Username
+  $RequestText = @"
+$MFARequest
+"@
+}
+else {
+  $RequestText = @"
+  <form action="create" method=POST>
+    <label for="Username">Username</label><br>
+    <input type="text" id="username" value="user@user.com"><br>
+    Use the Create button below to generate a MFA push towards the user.<br>
+    <input class="button" name="Submit" type="submit" value="Create">
+  </form>
+"@
+}
+
+# Interact with query parameters or the body of the request.
+$Body = @"
+<!DOCTYPE html>
+<html>
+<style>
+input#Username, select {
+  width: 70%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+.button {
+  width: 25%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #45a049;
+  width: 25%
+}
+.divider{
+  width:5px;
+  height:auto;
+}
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 40%
+}
+</style>
+<body>
+<center>
+$($Logo)
+<title>Password Push Portal</Title>
+<h3>Generate a one-time password URL</h3>
+
+<div>
+$RequestText
+</div>
+  </center>
+</body>
+</html>
+"@
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode  = [HttpStatusCode]::OK
+    Body        = $Body
+    ContentType = 'text/html'
+  })
---
+++ PSfunctions.ps1
@@ -0,0 +1,55 @@
+function New-MFARequest {
+    param (
+        [string]$EmailToPush
+    )
+
+    ######### Secrets #########
+    $ApplicationId = $ENV:ApplicationID
+    $ApplicationSecret = $ENV:ApplicationSecret
+    $RefreshToken = $ENV:Refreshtoken
+    $Genpass = $ENV:GeneratedPassword
+    ######### Secrets #########
+
+    write-host "Creating credentials and tokens." -ForegroundColor Green
+    $credential = New-Object System.Management.Automation.PSCredential($ApplicationId, ($ApplicationSecret | Convertto-SecureString -AsPlainText -Force))
+    $aadGraphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.windows.net/.default' -ServicePrincipal
+    $graphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.microsoft.com/.default' -ServicePrincipal
+    write-host "Connecting to MSOL" -ForegroundColor Green
+
+    Connect-MsolService -AdGraphAccessToken $aadGraphToken.AccessToken -MsGraphAccessToken $graphToken.AccessToken
+    $UserTenantName = $EmailToPush -split '@' | Select-Object -last 1
+    $UserTenantGUID = (Invoke-WebRequest "https://login.windows.net/$UserTenantName/.well-known/openid-configuration" | ConvertFrom-Json).token_endpoint.Split('/')[3] 
+    $MFAAppID = '981f26a1-7f43-403b-a875-f8b09b8cd720'
+    write-host "Setting temporary password" -ForegroundColor Green
+    New-MsolServicePrincipalCredential -TenantId $UserTenantGUID -AppPrincipalId $MFAAppID -Type password -Usage verify -Value $GenPass -Verbose
+    write-host "Generating XML" -ForegroundColor Green
+
+    $XML = @"
+<BeginTwoWayAuthenticationRequest>
+<Version>1.0</Version>
+<UserPrincipalName>$EmailToPush</UserPrincipalName>
+<Lcid>en-us</Lcid><AuthenticationMethodProperties xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays"><a:KeyValueOfstringstring><a:Key>OverrideVoiceOtp</a:Key><a:Value>false</a:Value></a:KeyValueOfstringstring></AuthenticationMethodProperties><ContextId>69ff05bf-eb61-47f7-a70e-e7d77b6d47d0</ContextId>
+<SyncCall>true</SyncCall><RequireUserMatch>true</RequireUserMatch><CallerName>radius</CallerName><CallerIP>UNKNOWN:</CallerIP></BeginTwoWayAuthenticationRequest>
+"@
+
+    $body = @{
+        'resource'      = 'https://adnotifications.windowsazure.com/StrongAuthenticationService.svc/Connector'
+        'client_id'     = $MFAAppID
+        'client_secret' = $Genpass
+        'grant_type'    = "client_credentials"
+        'scope'         = "openid"
+    }
+
+    $ClientToken = Invoke-RestMethod -Method post -Uri "https://login.microsoftonline.com/$UserTenantGUID/oauth2/token" -Body $body
+    $headers = @{ "Authorization" = "Bearer $($ClientToken.access_token)" }
+    write-host "Generating MFA Request" -ForegroundColor Green
+
+    $obj = Invoke-RestMethod -uri 'https://adnotifications.windowsazure.com/StrongAuthenticationService.svc/Connector//BeginTwoWayAuthentication' -Method POST -Headers $Headers -Body $XML -ContentType 'application/xml'
+
+    if ($obj.BeginTwoWayAuthenticationResponse.AuthenticationResult -ne $true) {
+        return "Authentication failed. does the user have Push/Phone call MFA configured? Errorcode: $($obj.BeginTwoWayAuthenticationResponse.result)"
+    }
+    if ($obj.BeginTwoWayAuthenticationResponse.result) {
+        return "Success"
+    }
+}
